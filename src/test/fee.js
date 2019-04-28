1159759d51ad23bd900b637f64f5a5285ded1d47
niels1286 <niels0519@gmail.com>
2019-04-28 14:11:01 +0800

    for new protocol

---
+++ src/test/fee.js
@@ -0,0 +1,7 @@
+//使用方式：先组装交易，然后获取手续费，再把手续费增加到from中，然后对交易进行签名，请确认交易地址有足够的余额。
+//signatrueCount 签名数量、默认为1
+export function countFee(tx, signatrueCount) {
+    let txSize = tx.txSerialize().length;//后面花些时间把tx.size()方法实现后，可以用size()
+    txSize += signatrueCount * 110;
+    return 100000 * (txSize / 1024);
+}
