ca673a6d870732a5669cf6a09d56c1293c85db86
MIMIEYES <pierreluohao@gmail.com>
2019-06-24 13:43:03 +0800

    add ecies crypto

---
+++ karma.conf.js
@@ -0,0 +1,72 @@
+process.env.CHROME_BIN = "chromium-browser";
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: "src/test",
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ["mocha", "browserify"],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      "cryptoTest.js"
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      "cryptoTest.js": ["browserify"],
+    },
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // test results reporter to use
+    // possible values: "dots", "progress"
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ["mocha"],
+
+
+    // web server port
+    port: 9879,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ["Chromium"],
+    customLaunchers: {
+      Chromium: {
+        base: "Chrome",
+        flags: ["--no-sandbox"],
+      },
+    },
+
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+  });
+};
