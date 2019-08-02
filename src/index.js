33040cda30cd94827575c3ec2f2dce14569b5e74
niels1286 <niels0519@gmail.com>
2019-08-02 11:11:33 +0800

    add address prefix

--- src/index.js
+++ src/index.js
@@ -4,143 +4,143 @@ const crypto = require("./crypto/eciesCrypto");
 
 module.exports = {
 
-  /**
-   * 生成地址
-   * @param chainId
-   * @param passWord
-   * @returns {{}}
-   */
-  newAddress(chainId, passWord) {
-    let addressInfo = {};
-    if (passWord) {
-      addressInfo = sdk.newEcKey(passWord);
-      addressInfo.aesPri = sdk.encrypteByAES(addressInfo.pri, passWord);
-      addressInfo.pri = null;
-    } else {
-      addressInfo = sdk.newEcKey(passWord);
+    /**
+     * 生成地址
+     * @param chainId
+     * @param passWord
+     * @returns {{}}
+     */
+    newAddress(chainId, passWord, prefix) {
+        let addressInfo = {"prefix": prefix};
+        if (passWord) {
+            addressInfo = sdk.newEcKey(passWord);
+            addressInfo.aesPri = sdk.encrypteByAES(addressInfo.pri, passWord);
+            addressInfo.pri = null;
+        } else {
+            addressInfo = sdk.newEcKey(passWord);
+        }
+        addressInfo.address = sdk.getStringAddress(chainId, addressInfo.pri, addressInfo.pub, prefix);
+        return addressInfo
+    },
+
+    /**
+     * 根据公钥获取地址
+     * @param chainId
+     * @param assetId
+     * @param pub
+     * @returns {*|string}
+     */
+    getAddressByPub(chainId, assetId, pub, prefix) {
+        return sdk.getStringAddressBase(chainId, assetId, '', pub, prefix);
+    },
+
+    /**
+     * 验证地址
+     * @param address
+     * @returns {*}
+     */
+    verifyAddress(address) {
+        return sdk.verifyAddress(address);
+    },
+
+    /**
+     * 导入地址
+     * @param chainId
+     * @param pri
+     * @param passWord
+     * @returns {{}}
+     */
+    importByKey(chainId, pri, passWord) {
+        let addressInfo = {};
+        addressInfo.pri = pri;
+        addressInfo.address = sdk.getStringAddress(chainId, pri);
+        addressInfo.pub = sdk.getPub(pri);
+        if (passWord) {
+            addressInfo.aesPri = sdk.encrypteByAES(addressInfo.pri, passWord);
+            addressInfo.pri = null;
+        }
+        return addressInfo
+    },
+
+    /**
+     * 组装交易
+     * @param inputs
+     * @param outputs
+     * @param remark
+     * @param type
+     * @param info
+     * @returns {Array}
+     */
+    transactionAssemble(inputs, outputs, remark, type, info) {
+        let tt = [];
+        if (type === 2) { //转账交易
+            tt = new txs.TransferTransaction();
+        } else if (type === 3) { //设置别名
+            tt = new txs.AliasTransaction(info.fromAddress, info.alias);
+        } else if (type === 4) { //创建节点
+            tt = new txs.CreateAgentTransaction(info);
+        } else if (type === 5) { //加入共识
+            tt = new txs.DepositTransaction(info);
+        } else if (type === 6) { //退出共识
+            tt = new txs.WithdrawTransaction(info);
+        } else if (type === 9) { //注销节点
+            tt = new txs.StopAgentTransaction(info);
+        } else if (type === 15) { //创建合约
+            tt = new txs.CreateContractTransaction(info);
+        } else if (type === 16) { //调用合约
+            tt = new txs.CallContractTransaction(info);
+        } else if (type === 17) { //删除合约
+            tt = new txs.DeleteContractTransaction(info);
+        } else if (type === 10) { //跨链转账
+            tt = new txs.CrossChainTransaction();
+        }
+        tt.setCoinData(inputs, outputs);
+        tt.remark = remark;
+        return tt
+    },
+
+    /**
+     * 交易签名
+     * @param pri
+     * @param pub
+     * @param tAssemble
+     * @returns {boolean}
+     */
+    transactionSerialize(pri, pub, tAssemble) {
+        sdk.signatureTx(tAssemble, pri, pub);
+        return tAssemble.txSerialize().toString('hex');
+    },
+
+    /**
+     * 交易签名
+     * @param pri
+     * @param tAssemble
+     * @returns {boolean}
+     */
+    transactionSignature(pri, tAssemble) {
+        return sdk.signatureTransaction(tAssemble, pri);
+    },
+
+    /**
+     * 解密私钥
+     * @param aesPri
+     * @param password
+     * @returns {*}
+     */
+    decrypteOfAES(aesPri, password) {
+        return sdk.decrypteOfAES(aesPri, password)
+    },
+
+    async encryptOfECIES(pub, data) {
+        let bufferData = Buffer.from(data);
+        let encrypted = await eccrypto.encrypt(pub, bufferData);
+        return encrypted.toString("hex");
+    },
+
+    async decryptOfECIES(pri, encrypted) {
+        let bufferEncrypted = Buffer.from(encrypted, "hex");
+        let decrypted = await eccrypto.decrypt(pri, bufferData);
+        return decrypted.toString();
     }
-    addressInfo.address = sdk.getStringAddress(chainId, addressInfo.pri, addressInfo.pub);
-    return addressInfo
-  },
-
-  /**
-   * 根据公钥获取地址
-   * @param chainId
-   * @param assetId
-   * @param pub
-   * @returns {*|string}
-   */
-  getAddressByPub(chainId, assetId, pub) {
-    return sdk.getStringAddressBase(chainId, assetId, '', pub);
-  },
-
-  /**
-   * 验证地址
-   * @param address
-   * @returns {*}
-   */
-  verifyAddress(address) {
-    return sdk.verifyAddress(address);
-  },
-
-  /**
-   * 导入地址
-   * @param chainId
-   * @param pri
-   * @param passWord
-   * @returns {{}}
-   */
-  importByKey(chainId, pri, passWord) {
-    let addressInfo = {};
-    addressInfo.pri = pri;
-    addressInfo.address = sdk.getStringAddress(chainId, pri);
-    addressInfo.pub = sdk.getPub(pri);
-    if (passWord) {
-      addressInfo.aesPri = sdk.encrypteByAES(addressInfo.pri, passWord);
-      addressInfo.pri = null;
-    }
-    return addressInfo
-  },
-
-  /**
-   * 组装交易
-   * @param inputs
-   * @param outputs
-   * @param remark
-   * @param type
-   * @param info
-   * @returns {Array}
-   */
-  transactionAssemble(inputs, outputs, remark, type, info) {
-    let tt = [];
-    if (type === 2) { //转账交易
-      tt = new txs.TransferTransaction();
-    } else if (type === 3) { //设置别名
-      tt = new txs.AliasTransaction(info.fromAddress, info.alias);
-    } else if (type === 4) { //创建节点
-      tt = new txs.CreateAgentTransaction(info);
-    } else if (type === 5) { //加入共识
-      tt = new txs.DepositTransaction(info);
-    } else if (type === 6) { //退出共识
-      tt = new txs.WithdrawTransaction(info);
-    } else if (type === 9) { //注销节点
-      tt = new txs.StopAgentTransaction(info);
-    } else if (type === 15) { //创建合约
-      tt = new txs.CreateContractTransaction(info);
-    } else if (type === 16) { //调用合约
-      tt = new txs.CallContractTransaction(info);
-    } else if (type === 17) { //删除合约
-      tt = new txs.DeleteContractTransaction(info);
-    } else if (type === 10) { //跨链转账
-      tt = new txs.CrossChainTransaction();
-    }
-    tt.setCoinData(inputs, outputs);
-    tt.remark = remark;
-    return tt
-  },
-
-  /**
-   * 交易签名
-   * @param pri
-   * @param pub
-   * @param tAssemble
-   * @returns {boolean}
-   */
-  transactionSerialize(pri, pub, tAssemble) {
-    sdk.signatureTx(tAssemble, pri, pub);
-    return tAssemble.txSerialize().toString('hex');
-  },
-
-  /**
-   * 交易签名
-   * @param pri
-   * @param tAssemble
-   * @returns {boolean}
-   */
-  transactionSignature(pri, tAssemble) {
-    return sdk.signatureTransaction(tAssemble, pri);
-  },
-
-  /**
-   * 解密私钥
-   * @param aesPri
-   * @param password
-   * @returns {*}
-   */
-  decrypteOfAES(aesPri, password) {
-    return sdk.decrypteOfAES(aesPri, password)
-  },
-
-  async encryptOfECIES(pub, data) {
-    let bufferData = Buffer.from(data);
-    let encrypted = await eccrypto.encrypt(pub, bufferData);
-    return encrypted.toString("hex");
-  },
-
-  async decryptOfECIES(pri, encrypted) {
-    let bufferEncrypted = Buffer.from(encrypted, "hex");
-    let decrypted = await eccrypto.decrypt(pri, bufferData);
-    return decrypted.toString();
-  }
 
 };
--- src/test/newAddress.js
+++ src/test/newAddress.js
@@ -1,10 +1,10 @@
 const nuls = require('../index');
 let passWord = '123456asd';//密码为空 私钥会返回
-const newAddress = nuls.newAddress(2, passWord);
+const newAddress = nuls.newAddress(3, passWord,'help');
 console.log(newAddress);
 let result = nuls.verifyAddress(newAddress.address);
 console.log(result);
 
 //根据公钥获取地址
-let  address = nuls.getAddressByPub(100,1,'0298f88c3cae67385ce3cbee00f78816db3e56e566b62bd0f4c5b45f205d3021c3');
+let  address = nuls.getAddressByPub(100,1,'0298f88c3cae67385ce3cbee00f78816db3e56e566b62bd0f4c5b45f205d3021c3',"help");
 console.log(address);
