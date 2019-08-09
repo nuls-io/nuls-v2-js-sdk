e6d33487b04353c1ebc901c6f05a5b5881974a25
niels1286 <niels0519@gmail.com>
2019-08-09 21:16:04 +0800

    add address equals method

---
+++ src/test/coindata.js
@@ -0,0 +1,29 @@
+const tx = require('../model/txs')
+let inputs = [];
+inputs.push({
+    address:'LINcjJR16WwP7a7irS3T4p1td7QAd2MSUEqk',
+    assetsChainId: 8,
+    assetsId: 1,
+    amount: 3300000000,
+    locked: 0,
+    nonce: '9673f2fdd28de29f'
+});
+inputs.push({
+    address:'LINcjJR16WwP7a7irS3T4p1td7QAd2MSUEqk',
+    assetsChainId: 2,
+    assetsId: 1,
+    amount: 1000000,
+    locked: 0,
+    nonce: '9673f2fdd28de29f'
+});
+let outputs = [];
+outputs.push({
+    address:'tNULSeBaMvEtDfvZuukDf2mVyfGo3DdiN8KLRG',
+    assetsChainId: 8,
+    assetsId: 1,
+    amount: 3300000000,
+    lockTime: 0
+});
+let temp = new tx.CrossChainTransaction();
+temp.setCoinData(inputs,outputs)
+console.log(temp.coinData.toString('hex'))
--- src/test/newAddress.js
+++ src/test/newAddress.js
@@ -1,13 +1,20 @@
 const nuls = require('../index');
+const sdk = require('../api/sdk')
 let passWord = '123456asd';//密码为空 私钥会返回
-const newAddress = nuls.newAddress(3, passWord, 'semo');
+const newAddress = nuls.newAddress(8, passWord,'LIN');
 console.log(newAddress);
 let result = nuls.verifyAddress(newAddress.address);
 console.log(result);
 
 //根据公钥获取地址
-let address = nuls.getAddressByPub(100, 1, '0298f88c3cae67385ce3cbee00f78816db3e56e566b62bd0f4c5b45f205d3021c3', "semo");
+let address = nuls.getAddressByPub(8, 1, newAddress.pub,'lin');
 console.log(address);
+console.log(address == newAddress.address)
+
+
+let hex = sdk.getBytesAddress(address).toString('hex')
+console.log(hex)
+
 
 //1.0与2.0私钥或公钥生成的地址是否相同
 let rest = nuls.addressEquals("TTarYnUfsftmm7DrStandCEdd4SNiELS", "tNULSeBaMoG1oaW1JZnh6Ly65Ttp6raeTFBfCG");
