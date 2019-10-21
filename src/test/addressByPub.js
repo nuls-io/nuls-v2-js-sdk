d7a7c85c1037dacac5105c6b6b9f34a2050f2310
wave <986887991@qq.com>
2019-10-21 09:07:31 +0800

    code

--- src/test/addressByPub.js
+++ src/test/addressByPub.js
@@ -5,6 +5,7 @@ const nuls = require('../index');
  * @date: 2019-10-18 10:27
  * @author: Wave
  */
+
 let addressInfo = {
   pub: '02458d425e75a95688571c4cc074289a34d27c74cd284d6be316a775b363a205c3',
   address: 'tNULSeBaMiv3V2KMKbkHL8ZRgQRkP6CYUG2hia',
