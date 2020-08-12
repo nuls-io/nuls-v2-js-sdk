7db8d14afef72c9d0fb1a0a52d2683834a05304f
wave <wave98688@gmail.com>
2020-08-12 15:47:43 +0800

    追加签名

--- src/test/flash_test.js
+++ src/test/flash_test.js
@@ -1,52 +1,39 @@
-const Serializers = require("../api/serializers");
-const BufferReader = require("../utils/bufferreader")
-const txs = require("../model/txs")
-const txsignatures = require("../model/txsignatures")
+const BufferReader = require("../utils/bufferreader");
+const txs = require("../model/txs");
+const txsignatures = require("../model/txsignatures");
 const nuls = require('../index');
-const sdk = require("../api/sdk")
-
-
-
+const sdk = require("../api/sdk");
 
 //todo 入参
-var txHex = "";
+let txHex = "";
 //todo 入参
-var prikeyHex = "";
+let prikeyHex = "";
 // 解析交易
-var bufferReader = new BufferReader(Buffer.from(txHex, "hex"), 0);
+let bufferReader = new BufferReader(Buffer.from(txHex, "hex"), 0);
 // 反序列回交易对象
 let tx = new txs.Transaction();
 tx.parse(bufferReader);
-
 // 初始化签名对象
 let txSignData = new txsignatures.TransactionSignatures();
-
 // 反序列化签名对象
 let reader = new BufferReader(tx.signatures, 0);
 txSignData.parse(reader);
-
 // 打印已签名地址
-let address = nuls.getAddressByPub(5, 1,txSignData.signatures[0].pubkey, 'TNVT');
-console.log(address)
-
+let address = nuls.getAddressByPub(5, 1, txSignData.signatures[0].pubkey, 'TNVT');
+console.log(address);
 //获取本账户公钥
-var pub = sdk.getPub(prikeyHex);
-
+let pub = sdk.getPub(prikeyHex);
 // 签名
 let sigHex = sdk.signature(tx.getHash().toString("hex"), prikeyHex);
 let signValue = Buffer.from(sigHex, 'hex');
-
 // 追加签名到对象中
-txSignData.addSign(Buffer.from(pub,"hex"),signValue)
-
+txSignData.addSign(Buffer.from(pub, "hex"), signValue);
 // 追加签名到交易中
 tx.signatures = txSignData.serialize();
-
 //计算交易hash
 tx.calcHash();
-
-console.log(tx.getHash().toString("hex"))
+console.log(tx.getHash().toString("hex"));
 // console.log(pub)
 // console.log(signValue)
 // 结果
-console.log(tx.txSerialize().toString("hex"))
+console.log(tx.txSerialize().toString("hex"));
