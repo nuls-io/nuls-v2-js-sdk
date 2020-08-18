cc208cdc153d3fca1e001fbe7c457780523bd96a
wave <wave98688@gmail.com>
2020-08-18 14:55:15 +0800

    添加私钥16进制验证

--- src/test/importAddress.js
+++ src/test/importAddress.js
@@ -7,7 +7,13 @@ const nuls = require('../index');
  */
 
 let passWord = '';
-const key = "30bfde504d8ff3c269d0a0816034c3eeceb2eaa04c1f0d36ee724527b0e3a25d";
-const importAddress = nuls.importByKey(100, key, passWord, "XXX");
-console.log(importAddress);
-console.log(importAddress.address === 'tNULSeBaMuBCG7NkSyufjE76CVbPQMrZ5Q1v3s');
+const key = "ddddb7cb859a467fbe05d5034735de9e62ad06db6557b64d7c139b6db856b200";
+try {
+  const importAddress = nuls.importByKey(2, key, passWord, "tNULS");
+  console.log(importAddress);
+  console.log(importAddress.address === 'tNULSeBaMuBCG7NkSyufjE76CVbPQMrZ5Q1v3s');
+} catch (err) {
+  console.log(err);
+}
+
+
